f632b3e8a6a90bca00b354eaa5c825dd1c92b779
Matt Insler <matt.insler@gmail.com>
2015-04-20 14:09:38 -0700

    fully working mysql storage

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE tasks;
 
-GRANT ALL ON tasks.* TO 'root'@'%' IDENTIFIED BY 'password';
+GRANT ALL ON tasks.* TO 'read-write'@'%' IDENTIFIED BY '8fdc21e776def2471015f6fcfc0663c942c7b18e593a4b955d30a208c837c32a';
 
 CREATE TABLE tasks (
   id BINARY(16) NOT NULL PRIMARY KEY,
@@ -23,7 +23,7 @@ CREATE TABLE executions (
   reaped_count SMALLINT DEFAULT 0,
   
   success BOOL,
-  error TEXT,
+  error_id BINARY(32), -- SHA256 hash of error text
   
   PRIMARY KEY (task_id, execution_id),
   CONSTRAINT fk_executions_task_id FOREIGN KEY (task_id) REFERENCES tasks (id) ON DELETE CASCADE
@@ -36,3 +36,17 @@ CREATE TABLE task_tags (
   PRIMARY KEY (id, task_id),
   CONSTRAINT fk_task_tags_task_id FOREIGN KEY (task_id) REFERENCES tasks (id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE execution_errors (
+  id BINARY(32) NOT NULL PRIMARY KEY, -- SHA256 hash of error text
+  error TEXT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE OR REPLACE VIEW task_executions AS
+  SELECT
+    t.id as task_id, t.data, t.queue, t.retry_queue,
+    ex.execution_id, ex.queued_at, ex.started_at, ex.finished_at, ex.failed_count, ex.reaped_count, ex.success,
+    er.error
+  FROM tasks t
+    JOIN executions ex ON (ex.task_id = t.id)
+    LEFT JOIN execution_errors er ON (er.id = ex.error_id);
