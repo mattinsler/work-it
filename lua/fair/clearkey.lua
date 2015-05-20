170be30f8757e095d5ed5e04fa5bb8a41ae0e848
Matt Insler <matt.insler@gmail.com>
2015-05-20 13:30:24 -0400

    added clearFairKey for fair queeus

---
+++ lua/fair/clearkey.lua
@@ -0,0 +1,14 @@
+-- KEYS:
+--     1: <the fair queue>
+-- ARGV:
+--     1: <the fair key to clear>
+
+
+local ids = redis.call('LRANGE', 'q:' .. ARGV[1], 0, -1)
+for x = 1, #ids do
+  ids[x] = 't:' .. ids[x]
+end
+
+redis.call('LREM', KEYS[1], 0, ARGV[1])
+redis.call('DEL', 'q:' .. ARGV[1])
+return redis.call('DEL', unpack(ids))
